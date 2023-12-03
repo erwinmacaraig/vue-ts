c7471fcb96fa58c409fe9192e12f216a7f5db65a
Erwin Macaraig <erwin.macaraig@gmail.com>
2023-12-03 20:38:52 +0800

    creating store for user

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -73,7 +73,9 @@
 </template>
 
 <script>
-import { auth, db } from '@/includes/firebase';
+import { auth, usersCollection } from '@/includes/firebase';
+import { mapWritableState } from 'pinia';
+import useUserStore from "@/stores/user";
 export default {
   name: "RegisterForm",
   data() {
@@ -106,17 +108,35 @@ export default {
       let userCred = null;
       try {
         userCred = await auth.createUserWithEmailAndPassword(values.email, values.password);
+
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = 'bg-red-500'
         this.reg_alert_msg = 'An unexpected error occured. Please try again later';
         return;
       }
+      try {
+        await usersCollection.add({
+          name: values.name,
+          email: values.email,
+          age: values.age,
+          country: values.country
+        });
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = 'bg-red-500'
+        this.reg_alert_msg = 'An unexpected error occured. Please try again later';
+        return;
+      }
+      this.userLoggedIn = true;
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
       console.log(userCred);
 
     },
   },
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"])
+  }
 };
 </script>
