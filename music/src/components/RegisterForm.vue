96867962b71fad269fd494079f4b4d48dc4a7cc0
Erwin Macaraig <erwin.macaraig@gmail.com>
2023-12-03 21:02:18 +0800

    refactor logic for authentication

--- music/src/components/RegisterForm.vue
+++ music/src/components/RegisterForm.vue
@@ -73,8 +73,7 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase';
-import { mapWritableState } from 'pinia';
+import { mapActions } from 'pinia';
 import useUserStore from "@/stores/user";
 export default {
   name: "RegisterForm",
@@ -100,14 +99,14 @@ export default {
     };
   },
   methods: {
+    ...mapActions(useUserStore, { createuser: "register" }),
     async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_msg = "Please wait! Your account is being created.";
-      let userCred = null;
       try {
-        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password);
+        await this.createuser(values)
 
       } catch (error) {
         this.reg_in_submission = false;
@@ -115,28 +114,12 @@ export default {
         this.reg_alert_msg = 'An unexpected error occured. Please try again later';
         return;
       }
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country
-        });
-      } catch (error) {
-        this.reg_in_submission = false;
-        this.reg_alert_variant = 'bg-red-500'
-        this.reg_alert_msg = 'An unexpected error occured. Please try again later';
-        return;
-      }
-      this.userLoggedIn = true;
+
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
-      console.log(userCred);
 
     },
   },
-  computed: {
-    ...mapWritableState(useUserStore, ["userLoggedIn"])
-  }
+
 };
 </script>
